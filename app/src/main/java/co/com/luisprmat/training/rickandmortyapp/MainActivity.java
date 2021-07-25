ee2e359c92156a0f8100362a9349456f2598ecbe
Luis Antonio Parrado <luisprmat@gmail.com>
2021-07-24 22:26:32 -0500

    Install App Android 4.1

---
+++ app/src/main/java/co/com/luisprmat/training/rickandmortyapp/MainActivity.java
@@ -0,0 +1,14 @@
+package co.com.luisprmat.training.rickandmortyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
