fe568afc650bce2971ea1610ce4367a052cfb53f
Luis Antonio Parrado <luisprmat@gmail.com>
2021-07-26 22:33:27 -0500

    Finally show charcters list in RecyclerView

---
+++ app/src/main/java/co/com/luisprmat/training/rickandmortyapp/characters/CharacterAdapter.java
@@ -0,0 +1,67 @@
+package co.com.luisprmat.training.rickandmortyapp.characters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import co.com.luisprmat.training.rickandmortyapp.R;
+
+public class CharacterAdapter extends RecyclerView.Adapter<CharacterViewHolder> {
+    Context context;
+    List<Character> characters;
+
+    public CharacterAdapter(Context context, List<Character> characters) {
+        this.context = context;
+        this.characters = characters;
+    }
+
+    @NonNull
+    @Override
+    public CharacterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_character, parent, false);
+        return new CharacterViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CharacterViewHolder holder, int position) {
+        Character character = characters.get(position);
+
+        holder.llCharacterContainer.setOnClickListener(v -> {
+
+        });
+
+        Glide.with(context)
+                .load(character.getImage())
+                .into(holder.ivAvatar);
+
+        String name = context.getString(R.string.text_name, character.getName());
+        String status = context.getString(R.string.text_status, character.getStatus());
+        String species = context.getString(R.string.text_species, character.getSpecies());
+        String gender = context.getString(R.string.text_gender, character.getGender());
+
+        holder.tvName.setText(name);
+        holder.tvStatus.setText(status);
+        holder.tvSpecies.setText(species);
+        holder.tvGender.setText(gender);
+
+        if (character.getOrigin() != null) {
+            String origin = context.getString(R.string.text_origin, character.getOrigin().getName());
+            holder.tvOrigin.setText(origin);
+        } else {
+            holder.tvOrigin.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return characters.size();
+    }
+}
--- app/src/main/java/co/com/luisprmat/training/rickandmortyapp/characters/CharactersActivity.java
+++ app/src/main/java/co/com/luisprmat/training/rickandmortyapp/characters/CharactersActivity.java
@@ -7,6 +7,9 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import co.com.luisprmat.training.rickandmortyapp.BaseActivity;
 import co.com.luisprmat.training.rickandmortyapp.R;
 import co.com.luisprmat.training.rickandmortyapp.network.RMLoader;
@@ -17,18 +20,26 @@ import retrofit2.Response;
 public class CharactersActivity extends BaseActivity {
     RecyclerView rvCharacters;
     TextView tvEmptyList;
+    List<Character> characters = new ArrayList<>();
+    CharacterAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        rvCharacters = findViewById(R.id.rvCharacters);
-//        rvCharacters.setHasFixedSize(true);
-//
-//        tvEmptyList = findViewById(R.id.tvEmptyList);
-//
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-//        rvCharacters.setLayoutManager(linearLayoutManager);
+        setContentView(R.layout.activity_characters);
+
+        rvCharacters = findViewById(R.id.rvCharacters);
+        rvCharacters.setHasFixedSize(true);
+
+        tvEmptyList = findViewById(R.id.tvEmptyList);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvCharacters.setLayoutManager(linearLayoutManager);
+
+        adapter = new CharacterAdapter(this, characters);
+
+        rvCharacters.setAdapter(adapter);
 
         loadCharactersFromNetwork();
     }
@@ -40,12 +51,19 @@ public class CharactersActivity extends BaseActivity {
             @Override
             public void onResponse(Call<CharactersResponse> call, Response<CharactersResponse> response) {
                 hideProgress();
+                if (response.isSuccessful() && response.body().getResults() != null) {
+                    characters.clear();
+                    characters.addAll(response.body().getResults());
+                    adapter.notifyDataSetChanged();
+                } else {
+                    showOkDialog("Error cargando personajes");
+                }
             }
 
             @Override
             public void onFailure(Call<CharactersResponse> call, Throwable t) {
                 hideProgress();
-                showOkDialog("Error cargando personajes");
+                showOkDialog("Error cargando personajes r");
             }
         });
     }
